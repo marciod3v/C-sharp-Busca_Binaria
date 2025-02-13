38c88d08231d9b3e1785669c3512e4dbf62cc83d
Marcio <marcio.devcode@gmail.com>
2025-02-13 17:17:16 -0300

    vetor atualizado

--- Program.cs
+++ Program.cs
@@ -24,12 +24,12 @@ namespace buscaBinaria
             short repetir = 1;
             while (repetir == 1)
             {
-                int[] vetor = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
-                21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38,
-                39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56,
-                57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74,
-                75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90, 91, 92,
-                93, 94, 95, 96, 97, 98, 99, 100 };
+                int[] vetor = new int[1000];
+
+                for (int i = 0; i < vetor.Length; i++)
+                {
+                    vetor[i] = i + 1;
+                }
 
                 int inicial = vetor[0];
                 int final = vetor.Length - 1;
@@ -78,6 +78,7 @@ namespace buscaBinaria
                         }
                         Console.WriteLine("Este é o numero certo ? (1) sim (2) nao ");
                         r = short.Parse(Console.ReadLine());
+
                     }
                 }
                 Console.WriteLine("Número encontrado, gostaria de realizar o teste novamente ? (1) sim (2) não");
